26c494ee5c373d8c222d2db99bda99e3964cd569
Andrey Dubinin <rakdav@inbox.ru>
2020-03-21 18:15:19 +0200

    first

--- app/src/main/java/com/example/simple2/MainActivity.java
+++ app/src/main/java/com/example/simple2/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
     String[] countries={"Russia","USA","Canada","France"};
@@ -14,5 +17,13 @@ public class MainActivity extends ListActivity {
         setContentView(R.layout.activity_main);
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_gallery_item,countries);
         setListAdapter(adapter);
+        AdapterView.OnItemClickListener itemListener=new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"Choice "+adapterView.getItemAtPosition(i).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        getListView().setOnItemClickListener(itemListener);
     }
 }
